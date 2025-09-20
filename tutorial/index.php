1db87146366a40bceeb98467f8d472d81cdf5977
larissaboingg <larissa_boing@estudante.sesisenai.org.br>
2025-09-19 21:02:33 -0300

    Update index.php

--- tutorial/index.php
+++ tutorial/index.php
@@ -1,3 +1,6 @@
+<?php 
+    session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,11 +12,39 @@
 <body>
         <div class="container">
             <div class="box form-box">
+                <?php 
+                
+                 include("php/config.php")
+                 if(isset($_POST['submit'])){
+                    $email = mysqli_real_escape_string($con,$_POST['email']);
+                    $password = mysqli_real_escape_string($con,$_POST['password']);
+
+                    $result = mysqli_query($con,"SELECT * FROM users WHERE Email='$email' AND Password='$password'" ) or die("Select Error");
+                    $row = mysqli_fetch_assoc($result);
+
+                    if(is_array($row) && empty($row)){
+                        $_SESSION['valid'] = $row['Email'];
+                        $_SESSION['username'] = $row['Username'];
+                        $_SESSION['age'] = $row['Age'];
+                        $_SESSION['id'] = $row['Id'];
+                    }else{
+                        echo "<div class='message'>
+                            <p>Wrong Username or Password</p>
+                            </div> <br>";
+                        echo "<a href='index.php'><button>Go Back</button>";
+                    }
+                    if(isset($_SESSION['valid'])){
+                        header("Location: home.php");
+                    }
+                }else{
+                 
+
+                ?>
                 <header>Login</header>
                 <form action="" method="post">
                     <div class="field input">
-                        <label for="username">Username</label>
-                        <input type="text" name="username" id="username" autocomplete="off" required>
+                        <label for="email">Email</label>
+                        <input type="text" name="email" id="email" autocomplete="off" required>
                     </div>
 
                     <div class="field input">
@@ -30,6 +61,7 @@
                     </div>
                 </form>
             </div>
+            <?php } ?>
         </div>
 </body>
 </html>
